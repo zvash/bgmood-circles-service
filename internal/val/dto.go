cf359108d02f0eb05d3eb6224d70c2aa73440e02
Siavash Hekmatnia <siavash.hekmatnia@gmail.com>
2023-09-06 18:41:49 +0330

    fixed oneof validation issue

--- internal/val/dto.go
+++ internal/val/dto.go
@@ -4,7 +4,7 @@ type CreateCircleRequest struct {
 	Title       string `json:"title" validate:"required,alphanumunicode"`
 	Avatar      string `json:"avatar" validate:"required"`
 	Description string `json:"description" validate:"omitempty,alphanumunicode"`
-	CircleType  string `json:"circle_type"  validate:"required,oneof=ROOM,HALL"`
+	CircleType  string `json:"circle_type"  validate:"required,oneof=ROOM HALL"`
 	IsPrivate   bool   `json:"is_private" validate:"required"`
 }
 
@@ -12,7 +12,7 @@ type EditCircleRequest struct {
 	Title       string `json:"title" validate:"omitempty,alphanumunicode"`
 	Avatar      string `json:"avatar" validate:"omitempty"`
 	Description string `json:"description" validate:"omitempty,alphanumunicode"`
-	CircleType  string `json:"circle_type"  validate:"omitempty,oneof=ROOM,HALL"`
+	CircleType  string `json:"circle_type"  validate:"omitempty,oneof=ROOM HALL"`
 	IsPrivate   bool   `json:"is_private" validate:"omitempty"`
 }
 
@@ -86,7 +86,7 @@ type GetCircleWPChangeAccessRequest struct {
 
 type SetCircleWPChangeAccessRequest struct {
 	CircleID   string `json:"circle_id" validate:"required,uuid"`
-	AccessType string `json:"access_type" validate:"required,oneof=ACCEPT_ALL,ASK_FIRST,REJECT_ALL"`
+	AccessType string `json:"access_type" validate:"required,oneof=ACCEPT_ALL ASK_FIRST REJECT_ALL"`
 }
 
 type ReactRequest struct {
